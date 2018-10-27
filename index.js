739da046cf74d596e252d78d5cff5586617bc97d
unibtc <unibtc@gmail.com>
2018-10-27 17:53:34 +0800

    check if user token already exists
    Signed-off-by: unibtc <unibtc@gmail.com>

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const ghGot = require('gh-got');
+const fs = require('fs');
+const gitCredsDir = `${process.env.HOME}/.git-credentials`;
+
+const checkGitCreds = function(user){
+  const readCreds = fs.readFileSync(gitCredsDir, 'utf8');
+  const users = readCreds.split('\n').map(i => i.split(':')[0]);
+  return users.includes(user);
+};
 
 exports.login = async (username, password, otp) => {
+  if(checkGitCreds(username)){
+    console.error('Already logged in');
+    process.exit(1);
+  }
   let headers = {
-    'accept': 'application/json',
-    'User-Agent': 'GitPodCe v1.0'
+    'accept': 'application/vnd.github.v3+json',
+    'User-Agent': 'GitPodCe v1.0',
     'Authorization': `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`
   };
   if (otp) {
@@ -20,4 +32,11 @@ exports.login = async (username, password, otp) => {
       'note_url': 'https://git.unibtc.me',
     }
   });
-}
+  if (body.token) {
+    fs.writeFile(`${process.env.HOME}/.git-credentials`, `${username}:${body.token}@https://api.github.com`, (err) => {
+      if (err) throw err;
+      console.log('Git token was saved at /home/user/.git-credentials');
+    });
+  }
+
+};
