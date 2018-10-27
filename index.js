b0ccbf983610ba0d854feaa923e3f0e8deac3eeb
unibtc <unibtc@gmail.com>
2018-10-27 17:03:05 +0800

    initial login export
    Signed-off-by: unibtc <unibtc@gmail.com>

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 const ghGot = require('gh-got');
+
+exports.login = async (username, password, otp) => {
+  let headers = {
+    'accept': 'application/json',
+    'User-Agent': 'GitPodCe v1.0'
+    'Authorization': `Basic ${Buffer.from(`${username}:${password}`).toString('base64')}`
+  };
+  if (otp) {
+    headers = Object.assign({}, headers, {
+      'X-GitHub-OTP': otp
+    });
+  }
+  const { body } = await ghGot('authorizations', {
+    json: true,
+    headers,
+    body: {
+      'scopes': ['repo'],
+      'note': 'GitpodCe',
+      'note_url': 'https://git.unibtc.me',
+    }
+  });
+}
